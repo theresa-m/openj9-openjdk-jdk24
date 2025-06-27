45c2012af83eaf70076300c11ff34f296c0bf807
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2025-06-27 14:00:53 -0400

    Revert "Return Thread.State.RUNNABLE for VirtualThread.BLOCKING"
    This reverts commit cd6f4212beaf5e7aadcef0d693282cfc4d924f91.

--- src/java.base/share/classes/java/lang/VirtualThread.java
+++ src/java.base/share/classes/java/lang/VirtualThread.java
@@ -1130,7 +1130,6 @@ final class VirtualThread extends BaseVirtualThread {
                 }
                 // runnable, mounted
                 return Thread.State.RUNNABLE;
-            case BLOCKING:
             case PARKING:
             case TIMED_PARKING:
             case WAITING:
@@ -1146,6 +1145,7 @@ final class VirtualThread extends BaseVirtualThread {
             case TIMED_PINNED:
             case TIMED_WAIT:
                 return Thread.State.TIMED_WAITING;
+            case BLOCKING:
             case BLOCKED:
                 return Thread.State.BLOCKED;
             case TERMINATED:
