3a7489d9a2b83030122adc2feacd11966693a5d3
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2025-07-11 10:25:43 -0400

    try short timeout - 2400 seconds

--- test/jdk/java/lang/Thread/virtual/CancelTimerWithContention.java
+++ test/jdk/java/lang/Thread/virtual/CancelTimerWithContention.java
@@ -27,7 +27,7 @@
  *     contention on the timer queue
  * @requires vm.continuations
  * @key randomness
- * @run main/othervm/timeout=2880 
+ * @run main/othervm/timeout=300 
  *     -Djdk.virtualThreadScheduler.parallelism=2
  *     -Djdk.virtualThreadScheduler.timerQueues=1
  *     CancelTimerWithContention
