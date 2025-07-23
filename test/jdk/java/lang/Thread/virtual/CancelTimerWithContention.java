00abcde460abc0a0b816629336b06f2b55df6deb
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2025-07-23 08:34:28 -0400

    try options

--- test/jdk/java/lang/Thread/virtual/CancelTimerWithContention.java
+++ test/jdk/java/lang/Thread/virtual/CancelTimerWithContention.java
@@ -30,6 +30,7 @@
  * @run main/othervm
  *     -Djdk.virtualThreadScheduler.parallelism=2
  *     -Djdk.virtualThreadScheduler.timerQueues=1
+ *     -Xjit:disableLocalCSE  -Xjit:disableDynamicLoopTransfer -XX:+MergeCompilerOptions
  *     CancelTimerWithContention
  */
 
